59212217200076f16454053105983000903ec023
Maria <mariajesus130402@gmail.com>
2024-05-05 12:25:54 -0600

    implementación de librerias y slide de tareas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ['./build/*.html', "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
